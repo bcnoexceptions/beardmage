01b1944cf5d6686245450481b3205b81066f7614
bcnoexceptions <crr65536@gmail.com>
2019-12-11 20:30:18 -0600

    Add !subject

---
+++ src/commands/subject.ts
@@ -0,0 +1,14 @@
+import * as Discord from "discord.js";
+import { getUserName } from "../knownUsers";
+
+export default function process(message: Discord.Message): void {
+    const newSubject = message.content.substring("!subject ".length);
+
+    const channel = message.channel as Discord.TextChannel;
+    channel.setTopic(newSubject);
+
+    const name = getUserName(message.member);
+    channel.send(`${name} changed the subject to "${newSubject}"`);
+}
+
+process.help = "change the subject";
