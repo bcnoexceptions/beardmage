e47543ec27f5fa25dcde6a905a8047e5217166c4
BC <bc@tenderloin>
2020-01-23 21:42:58 -0600

    Fix syntax errors

--- src/randChat.ts
+++ src/randChat.ts
@@ -2,30 +2,40 @@ import * as Discord from "discord.js";
 import { getUserName } from "./knownUsers";
 import { tryToPostInSameChannel } from "./channels";
 
-export function randChat(message: Discord.Message, start: string, middle: string, end: string, randAppend: string, punc: string, isCap: string, max?: number ) {
+export function randChat(
+	message: Discord.Message,
+	start: string,
+	middle: string,
+	end: string,
+	randAppend: string,
+	punc: string,
+	isCap: boolean,
+	max?: number
+) {
 	let result: string;
-	if ((!max) || (max===0)) {
+	if (!max || max === 0) {
 		max = 45;
 	}
 	let rNum = Math.floor(Math.random() * Math.floor(max)) + 1;
 	result = start;
-	for (int i = 0; i < rNum; i++) {
+	for (let i = 0; i < rNum; i++) {
 		result += middle;
 	}
 	result += end;
-	
+
 	rNum = Math.random();
-	
-	if (rNum < .125) {  //Same as 1/8 chance which is what is was in coolchat
+
+	if (rNum < 0.125) {
+		//Same as 1/8 chance which is what is was in coolchat
 		result += randAppend;
 	}
-	
+
 	rNum = Math.floor(Math.random() * 4) + 1;
-	
-	for (int i = 0; i < rNum; i++){
+
+	for (let i = 0; i < rNum; i++) {
 		result += punc;
 	}
-	
+
 	if (!isCap) {
 		result = result.toLowerCase();
 	}
