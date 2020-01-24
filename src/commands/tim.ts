acb80081e01dfa51493357d316b38fdc39520743
osumariokartman <osumariokartman@gmail.com>
2020-01-23 21:40:48 -0600

    Fix tim.ts to use quotes for import randChat

--- src/commands/tim.ts
+++ src/commands/tim.ts
@@ -1,5 +1,5 @@
 import * as Discord from "discord.js";
-import { randChat } from ../randChat.ts
+import { randChat } from "../randChat.ts"
 
 export default function process(message: Discord.Message): void {
     randChat(message, "D", "A", "NG", " GURRRRRRRL", "!", Math.random() < 0.5);
