b761b7bd8eefa6e647677f5f14e8b0be5da832a8
BC <bc@tenderloin>
2020-01-23 22:14:09 -0600

    Fix includes

--- src/commands/kelso.ts
+++ src/commands/kelso.ts
@@ -1,8 +1,8 @@
 import * as Discord from "discord.js";
-import { randChat } from "../randChat.ts"
+import { randChat } from "../randChat";
 
 export default function process(message: Discord.Message): void {
-    randChat(message, "B", "U", "RN", "!  EPIC BURN", "!", Math.random() < 0.5);
+	randChat(message, "B", "U", "RN", "!  EPIC BURN", "!", Math.random() < 0.5);
 }
 
 process.help = "!kelso gonna need some ointment for that burn";
--- src/commands/ptl.ts
+++ src/commands/ptl.ts
@@ -1,8 +1,8 @@
 import * as Discord from "discord.js";
-import { randChat } from "../randChat.ts"
+import { randChat } from "../randChat";
 
 export default function process(message: Discord.Message): void {
-    randChat(message, "G", "O", "LD", "", "!", Math.random() < 0.5);
+	randChat(message, "G", "O", "LD", "", "!", Math.random() < 0.5);
 }
 
 process.help = "!ptl always follows the golden rule";
