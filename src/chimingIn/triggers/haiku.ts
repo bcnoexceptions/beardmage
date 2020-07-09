32490eddcd0c2848f27154dc3b0b30b1bad37f64
BC Monoceros <crr65536@gmail.com>
2020-07-09 16:49:15 -0500

    Add multiline message haiku support

--- src/chimingIn/triggers/haiku.ts
+++ src/chimingIn/triggers/haiku.ts
@@ -17,28 +17,56 @@ export class ChatSlaveHaiku implements IBotTrigger {
 	}
 
 	public react(message: Discord.Message): void {
-		const syllableCount = syllable(message.content);
-		if (syllableCount === 0) {
-			return;
+		if (message.content && message.content[0] === "!") {
+			return; // bot command
 		}
 
 		const channelName = (message.channel as Discord.TextChannel).name;
+
 		if (!this.lastSyllablesByChannel[channelName]) {
 			this.lastSyllablesByChannel[channelName] = [];
 		}
-
 		const queue = this.lastSyllablesByChannel[channelName];
-		queue.push(syllableCount);
+
+		let haikuFound = false;
+		let lines = message.content.split(/[\r\n]+/g);
+
+		for (const line of lines) {
+			const syllableCount = syllable(line);
+			if (syllableCount === 0) {
+				continue;
+			}
+			if (this.pushOneMessage(queue, syllableCount)) {
+				haikuFound = true;
+			}
+		}
+
+		if (haikuFound) {
+			message.channel.send("nice haiku!");
+		}
+	}
+
+	/**
+	 * Add one line to the queue. Returns true if a haiku was found
+	 *
+	 * @param {number[]} queue
+	 * @param {number} syllables
+	 * @returns {boolean} true if a haiku was found
+	 */
+	private pushOneMessage(queue: number[], syllables: number): boolean {
+		queue.push(syllables);
 
 		if (queue.length > 3) {
 			queue.shift();
 		}
 		if (queue.length < 3) {
-			return;
+			return false;
 		}
 
 		if (queue[0] === 5 && queue[1] === 7 && queue[2] === 5) {
-			message.channel.send("nice haiku!");
+			return true;
+		} else {
+			return false;
 		}
 	}
 }
