0ec2c732895757956a02ceb9f2b6dcaf32010066
connork09 <connork09@gmail.com>
2020-08-12 22:49:09 -0400

    Fix whitespace.

--- src/commands/npr.ts
+++ src/commands/npr.ts
@@ -4,19 +4,21 @@ import { notifyAuthorOfFailure } from "../util";
 
 const NPR_FEED_BASE_URL = "https://feeds.npr.org/";
 const FEED_FILE_NAME = "/feed.json";
-const DEFUALT_FEED = 1001
+const DEFUALT_FEED = 1001;
 
 export default function process(message: Discord.Message): void {
-    getFromUrl(getFeedUrl(DEFUALT_FEED)).then((data) => {
-        try {
-          const feed = JSON.parse(data);
-          message.channel.send(getMessage(feed));
-        } catch (e) {
-          notifyAuthorOfFailure(message, e.message);
-        }
-    }).catch((reason) => {
-      notifyAuthorOfFailure(message, reason);
-    });
+    getFromUrl(getFeedUrl(DEFUALT_FEED))
+        .then((data) => {
+            try {
+                const feed = JSON.parse(data);
+                message.channel.send(getMessage(feed));
+            } catch (e) {
+                notifyAuthorOfFailure(message, e.message);
+            }
+        })
+        .catch((reason) => {
+            notifyAuthorOfFailure(message, reason);
+        });
 }
 
 function getMessage(feed: any): string {
@@ -27,7 +29,7 @@ function getMessage(feed: any): string {
 }
 
 function getFeedUrl(feedId: number): string {
-  return `${NPR_FEED_BASE_URL}${feedId}${FEED_FILE_NAME}`;
+    return `${NPR_FEED_BASE_URL}${feedId}${FEED_FILE_NAME}`;
 }
 
 process.help = "Get the latest news from NPR";
